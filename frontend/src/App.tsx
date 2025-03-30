222e5d041af6fe56c4737af2865af59e2f82820b
hugo566 <83823637+hugolgle@users.noreply.github.com>
2025-03-30 19:30:49 +0200

    feat/loginpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import MainLayout from "./components/layouts/MainLayout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   const router = createBrowserRouter([
     {
       element: <MainLayout />,
       errorElement: <div>Not Found</div>,
-      children: [{ path: "/", element: <Home /> }],
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/login", element: <Login /> },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
