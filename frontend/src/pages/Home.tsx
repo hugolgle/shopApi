c9d295509eb39e1cefc49225d2fd7597f796b8c2
Tanguy MLN <tanguy.malnory@delayed.co>
2025-04-06 17:38:07 +0200

    improve(footer, home): improve ui of the footer and homepage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -98,9 +98,9 @@ function Home() {
           </div>
         </div>
       </section>
-      <section className="flex flex-col pt-10 px-4">
+      <section className="flex flex-col pt-10">
         <div className="flex flex-col items-center py-4 w-full gap-4">
-          <div className="flex flex-col gap-2 w-full justify-between items-end lg:flex-row lg:px-20">
+          <div className="flex flex-col gap-2 w-full justify-between items-end lg:flex-row">
             <div className="flex flex-col gap-y-2 lg:w-3/5">
               <h4 className="text-gray-500 font-black">
                 COLLECTION DE SAC À DOS
@@ -164,10 +164,10 @@ function Home() {
           </div>
         </div>
       </section>
-      <section className="flex flex-col pt-10 px-4">
+      <section className="flex flex-col pt-10 mb-20">
         <div className="flex flex-col items-center py-4 w-full gap-4">
-          <div className="flex flex-col gap-2 w-full justify-between items-end lg:flex-row lg:px-20">
-            <div className="flex flex-col gap-y-2 lg:w-3/5">
+          <div className="flex flex-col gap-2 w-full justify-between items-end lg:flex-row">
+            <div className="flex flex-col gap-y-2">
               <h4 className="text-gray-500 font-black">ACCESSOIRES</h4>
               <h1 className="text-xl font-black font-boldonse">
                 ESSENTIELS POUR SAC À DOS
@@ -183,7 +183,7 @@ function Home() {
             </Button>
           </div>
           <div className="flex gap-10 justify-center items-center">
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-5">
               <CardProduct
                 img={bag}
                 name="Sac Louis Vuitton"
